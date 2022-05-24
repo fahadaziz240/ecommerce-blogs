23257cd4797c7aa80c47ec4caa44c5e66c93f7f3
Fahad Aziz <fahadaziz240@gmail.com>
2022-05-24 17:33:22 +0500

    commit

--- application/views/register/index.php
+++ application/views/register/index.php
@@ -2,27 +2,27 @@
     <div class="row">
         <div class="col-6 offset-md-3 text-center">
             <h1 class="mb-3">Register</h1>
-        </div>
-    </div>
-    <?php validation_errors() ?>
-    <form action="<?php base_url('register') ?>" method="POST" ?>
-        <div class="form-group">
-            <input type="text" class="form-control" required name="username" value="" placeholder="Username">
-        </div>
-        <div class="form-group">
-            <input type="email" class="form-control" required name="email" value="" placeholder="Email">
-        </div>
-        <div class="form-group">
-            <input type="password" class="form-control" required name="password" value="" placeholder="Password">
-        </div>
-        <div class="form-group">
-            <input type="password" class="form-control" required name="passconf" value="" placeholder="Confirm Password">
-        </div>
-        <button type="submit" class="btn btn-block btn-primary">Register</button>
-    </form>
-    <div class="row">
-        <div class="col-12">
-            <a class="btn btn-info mt-4" href="<?= base_url('login') ?>">Back to Login</a>
+            <?php validation_errors() ?>
+            <form action="<?php base_url('register') ?>" method="POST" ?>
+                <div class="form-group">
+                    <input type="text" class="form-control" required name="username" value="" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" required name="email" value="" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" required name="password" value="" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" required name="passconf" value="" placeholder="Confirm Password">
+                </div>
+                <button type="submit" class="btn btn-block btn-primary">Register</button>
+            </form>
+            <div class="row">
+                <div class="col-12">
+                    <a class="btn btn-info mt-4" href="<?= base_url('login') ?>">Back to Login</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
